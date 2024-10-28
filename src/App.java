8ad22c0352fc74dffde5d4e9635ecc3d7c926a03
AlexRam123-user <alex2004rx@gmail.com>
2024-10-28 12:55:27 -0500

    Recursividad

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Recursividad rec = new Recursividad();
+        int resultado = rec.factorial(5);
+        System.out.println(resultado);
+
+        int suma = rec.sumaConsecutivo(5);
+        System.out.println(suma);
+
+        int potencia = rec.potencia(3, 3);
+        System.out.println(potencia);
+
+        int sumaD= rec.sumarDigitos(456);
+        System.out.println(sumaD);
     }
 }
