5903cba337aba89db3ffe128602f677831ac1d26
AlexRam123-user <alex2004rx@gmail.com>
2024-10-30 12:33:12 -0500

    Recursividad Actualizado

--- src/RenombrarDirectorios.java
+++ src/RenombrarDirectorios.java
@@ -1,13 +1,32 @@
-import java.io.File;   
+import java.io.File;
 
 public class RenombrarDirectorios {
-    public void renombrarDirectorios (String path){
+    public void renombrarDirectorios(String path) {
         File directorio = new File(path);
-        if(!directorio.exists() || !directorio.isDirectory()){
+        if (!directorio.exists() || !directorio.isDirectory()) {
             System.out.println("La ruta path no es directorio valido");
             return;
         }
-        //ACA vamos a cambiar los nombres
-        System.out.println("La ruta path es valida");
+        // ACA vamos a cambiar los nombres
+        // System.out.println("La ruta path es valida");
+        renombrarDirectoriosInternos(directorio);
+    }
+
+    public void renombrarDirectoriosInternos(File directorio) {
+        // Listar todos los archivos y/o carpetas hijas
+        File[] directoriosArchivosInternos = directorio.listFiles();
+        if (directoriosArchivosInternos == null) {
+            return;
+        }
+        for (File directorioArchivo : directoriosArchivosInternos) {
+            if(directorioArchivo.isDirectory()){ // si es carpeta cambio el nombre
+                String nombreOriginal = directorioArchivo.getName();
+                String nuevoNombre = "Nuevo -"+nombreOriginal;
+                File nuevoDirectorio = new File(directorioArchivo.getParent(), nuevoNombre);
+                if(directorioArchivo.renameTo(nuevoDirectorio)){
+                    renombrarDirectoriosInternos(nuevoDirectorio);
+                }
+            }
+        }
     }
 }
